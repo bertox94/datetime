65e9a8714e793624c86851a011cece84fbbaedb4
bertox94 <10519773@polimi.it>
2022-02-21 23:14:10 +0100

    OK

--- classes.h
+++ classes.h
@@ -7,56 +7,31 @@
 
 using namespace std;
 
-
-class date {
-public:
-// since those can expresss also a duration, it make sense to use long long (e.g. when huge number of days)
-
-    unsigned int sec = NULL;
-    unsigned int min = NULL;
-    unsigned int hrs = NULL;
-    unsigned int day = NULL;
-    unsigned int month = NULL;
-    long long year = NULL;
-
-    bool operator==(date &other) const {
-        return sec == other.sec &&
-               min == other.min &&
-               hrs == other.hrs &&
-               day == other.day &&
-               month == other.month &&
-               year == other.year;
-    }
-
-};
-
-
 class datetime {
 public:
-    int a[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int days_of_months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    long long sec;
-    long long min;
-    long long hrs;
-    long long day;
-    long long month;
-    long long year;
+    long long sec{};
+    long long min{};
+    long long hrs{};
+    long long day{};
+    long long month{};
+    long long year{};
 
     datetime() = default;
 
-    datetime(long long int timestamp) {
+    explicit datetime(long long timestamp) {
         year = 1970 + timestamp / (((double) 146097 / 400) * 24 * 60 * 60);
         long long r1 = ((year - 1970) * 365 + (((year - 1) / 4 - 1970 / 4) - ((year - 1) / 100 - 1970 / 100) +
                                                ((year - 1) / 400 - 1970 / 400))) * 86400;
-
-        long long ddays = timestamp / (86400);
-        hrs = (timestamp - ddays * 86400) / 3600;
-        min = (timestamp - ddays * 86400 - hrs * 3600) / 60;
-        sec = timestamp - ddays * 86400 - hrs * 3600 - min * 60;
-
-        day = 0;
+        month = 1;
+        day = 1;
+        hrs = (timestamp - timestamp / (86400) * 86400) / 3600;
+        min = (timestamp - timestamp / (86400) * 86400 - hrs * 3600) / 60;
+        sec = timestamp - timestamp / (86400) * 86400 - hrs * 3600 - min * 60;
         timestamp -= hrs * 3600 + min * 60 + sec;
 
+        long long ddays;
         while (r1 > timestamp) {
             ddays = 365;
             if (((year - 1) % 400 == 0) || ((year - 1) % 4 == 0 && (year - 1) % 100 != 0))
@@ -64,21 +39,15 @@ public:
             r1 -= ddays * 86400;
             year--;
         }
-        //calcola il numero di anni bisestili, e aggiunge un numero di giorni pari al numeri di anni bisestili
-        r1 = ((year - 1970) * 365 + (((year - 1) / 4 - 1970 / 4) - ((year - 1) / 100 - 1970 / 100) +
-                                     ((year - 1) / 400 - 1970 / 400))) * 86400;
 
-
-        bool fg = false;
-        while (r1 <= timestamp) {
-            ddays = 365;
-            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-                ddays += 1;
-            r1 += ddays * 86400;
-            year++;
-            fg = true;
-        }
-        if (fg) {
+        if (r1 <= timestamp) {
+            while (r1 <= timestamp) {
+                ddays = 365;
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                    ddays += 1;
+                r1 += ddays * 86400;
+                year++;
+            }
             year--;
             ddays = 365;
             if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
@@ -88,37 +57,22 @@ public:
 
         bool leap = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
 
-        month = 1;
-        fg = false;
-        while (r1 <= timestamp) {
-            r1 += (leap && month == 2 ? 1 + a[month - 1] : a[month - 1]) * 86400;
-            month++;
-            fg = true;
-        }
-        if (fg) {
+        if (r1 <= timestamp) {
+            while (r1 <= timestamp) {
+                r1 += (leap && month == 2 ? 1 + days_of_months[month - 1] : days_of_months[month - 1]) * 86400;
+                month++;
+            }
             month--;
-            r1 -= (leap && month == 2 ? 1 + a[month - 1] : a[month - 1]) * 86400;
+            r1 -= (leap && month == 2 ? 1 + days_of_months[month - 1] : days_of_months[month - 1]) * 86400;
         }
 
-        day = 1;
-        fg = false;
-        while (r1 <= timestamp) {
-            r1 += 86400;
-            day++;
-            fg = true;
-        }
-        if (fg) {
+        if (r1 <= timestamp) {
+            while (r1 <= timestamp) {
+                r1 += 86400;
+                day++;
+            }
             day--;
         }
-    }
-
-    bool operator==(date &&dt) const {
-        return sec == dt.sec &&
-               min == dt.min &&
-               hrs == dt.hrs &&
-               day == dt.day &&
-               month == dt.month &&
-               year == dt.year;
     }
 };
 
