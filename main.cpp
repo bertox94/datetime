e75798cd3cf4f365a5546326ce02ac7b23a6eb61
bertox94 <10519773@polimi.it>
2022-02-23 02:31:59 +0100

    OK

--- main.cpp
+++ main.cpp
@@ -16,11 +16,16 @@ void test(long long size) {
             cout << "Error 2: " << -i << ", instead dt.timestamp()= " << datetime(-i).to_timestamp() << endl;
             return;
         }
-    }
-    auto t2 = chrono::high_resolution_clock::now();
 
+        if (i % 1000000 == 0)
+            cout << (long) (i / (double) size * 100) << " %" << endl;
+    }
+    cout << 100 << " %" << endl;
+
+    auto t2 = chrono::high_resolution_clock::now();
     chrono::duration<double, std::milli> ms_double = t2 - t1;
-    std::cout << ms_double.count() * 1000000 / size << " ns/op\n";
+    //std::cout << ms_double.count() * 1000000 / size << " ns/op\n";
+    //std::cout << (long) ms_double.count() << " ms\n";
 }
 
 std::ostream &operator<<(std::ostream &os, datetime const &d) {
@@ -34,7 +39,7 @@ int main() {
     //cout << datetime(94670856) << endl;
 
 
-    long long epoch = 30000000000;
+    long long epoch = 15000000000;
 
     test(epoch); //16700000
     cout << datetime(epoch) << endl;//30000000000
