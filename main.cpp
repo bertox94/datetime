45c389af700c89047242103ca6494a0aea2ed37d
bertox94 <10519773@polimi.it>
2022-03-14 22:56:19 +0100

    OK!!!!!! Super!!!  secondsto fixed

--- main.cpp
+++ main.cpp
@@ -9,13 +9,50 @@ using namespace std;
 
 void performance_test(long long size) {
     datetime dtp;
-    auto t1 = chrono::high_resolution_clock::now();
-    for (long long i = 0; i < size; i++)
-        dtp.after(i);
+    datetime av_compiler_opt;
 
+    auto t1 = chrono::high_resolution_clock::now();
+    for (long long i = 0; i < size; i++) {
+        av_compiler_opt = datetime(rand() - RAND_MAX / 2);
+        dtp.seconds_to(av_compiler_opt);
+        if (rand() % 345)
+            av_compiler_opt = datetime(rand() - RAND_MAX / 2);
+    }
     auto t2 = chrono::high_resolution_clock::now();
-    chrono::duration<double, std::milli> ms_double = t2 - t1;
-    std::cout << ms_double.count() * 1000000 / size << " ns/op\n";
+
+    auto t3 = chrono::high_resolution_clock::now();
+    for (long long i = 0; i < size; i++) {
+        av_compiler_opt = datetime(rand() - RAND_MAX / 2);
+        if (rand() % 345)
+            av_compiler_opt = datetime(rand() - RAND_MAX / 2);
+    }
+    auto t4 = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, std::milli> ms_double = t2 - t1 - (t4 - t3);
+    std::cout << "seconds_to(datetime): " << ms_double.count() * 1000000 / size << " ns/op .."
+              << av_compiler_opt << endl;
+
+    int avoid_compiler_optimization = 0;
+    t1 = chrono::high_resolution_clock::now();
+    for (long long i = 0; i < size; i++) {
+        avoid_compiler_optimization = rand() - RAND_MAX / 2;
+        dtp.after(avoid_compiler_optimization);
+        if (rand() % 345)
+            avoid_compiler_optimization = rand();
+    }
+    t2 = chrono::high_resolution_clock::now();
+
+    t3 = chrono::high_resolution_clock::now();
+    for (long long i = 0; i < size; i++) {
+        avoid_compiler_optimization = rand() - RAND_MAX / 2;
+        if (rand() % 345)
+            avoid_compiler_optimization = rand();
+    }
+    t4 = chrono::high_resolution_clock::now();
+
+    ms_double = t2 - t1 - (t4 - t3);
+    std::cout << "after(long long): " << ms_double.count() * 1000000 / size << " ns/op .."
+              << avoid_compiler_optimization << endl;
 }
 
 void test2(long long size) {
